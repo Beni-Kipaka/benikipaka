ee13b55af0c13fc432f76e4c7301fb0da7c0bdf1
Beni-Kipaka <benikipaka01@gmail.com>
2022-10-23 15:20:47 +0100

    Responsive

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -187,41 +187,15 @@ themeButton.addEventListener('click', () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*=====CHANGE BACKGROUND HEADER=====*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    //
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+                       : header.classList.remove('scroll-header')
+}
 
+window.addEventListener('scroll', scrollHeader)
 
 
 
